c2054ee816d61a330c902b8687f0feebaa824b6b
Anu Gulati <anu.gulati100@gmail.com>
2021-02-22 21:49:58 -0500

    Add tests for empty string and all headers

--- markdown_converter_test.rb
+++ markdown_converter_test.rb
@@ -54,4 +54,36 @@ What\'s going on?</p>
         html = MarkdownConverter.new(input_string).convert
         assert_equal html, expected
     end
+
+    def test_empty_markdown
+        input_string = '```
+        ```'
+        expected = '```
+        ```'
+        html = MarkdownConverter.new(input_string).convert
+        assert_equal html, expected
+    end
+
+    def test_all_headers
+        input_string = '```
+# This is an h1
+## This is an h2
+### This is an h3
+#### This is an h4
+##### This is an h5
+###### This is an h6
+        ```'
+
+        expected = '```
+<h1>This is an h1</h1>
+<h2>This is an h2</h2>
+<h3>This is an h3</h3>
+<h4>This is an h4</h4>
+<h5>This is an h5</h5>
+<h6>This is an h6</h6>
+        ```'
+
+        html = MarkdownConverter.new(input_string).convert
+        assert_equal html, expected
+    end
 end
